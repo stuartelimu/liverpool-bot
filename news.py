3450e0b39cbaf0a895e05a1f6a129fbf4eef448a
ElimuStuart <stuartelimu@gmail.com>
2019-11-23 01:57:59 +0300

    refactor code(DRY)

--- news.py
+++ news.py
@@ -5,39 +5,53 @@ import nltk
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://theathletic.com/'
+url = 'https://theathletic.com'
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
-def scrape_the_athletic():
-    HEADERS = {
+HEADERS = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)'
                       ' AppleWebKit/537.36 (KHTML, like Gecko) Cafari/537.36'
         }
-    r = requests.get(f'{url}author/james-pearce/', headers=HEADERS).text
+
+def extract_paratext(para_soup):
+    
+    para = para_soup.find('div', id='the_paywall').text
+    return tokenizer.tokenize(para)
+
+def extract_text(para_tokenized):
+    """Returns a sufficiently-large random text from a tokenized paragraph,
+    if such text exists. Otherwise, returns None."""
+
+    for _ in range(10):
+        text = random.choice(para_tokenized)
+        if text and 60 < len(text) < 210:
+            return text
+
+    return None
+
+def scrape_the_athletic():
+    """Scrapes content from The Athletic blog."""
+
+    r = requests.get(f'{url}/author/james-pearce/', headers=HEADERS).text
     soup = BeautifulSoup(r, 'lxml')
 
     latest_articles = soup.find_all(attrs={"data-object-type": "article", "class": "col-sm-3"})
 
     latest_article_links =  [latest_article.a['href'] for latest_article in latest_articles]
 
-    # latest_article_link = f"{url}{latest_article}"
 
     for link in latest_article_links:
-        r = requests.get(f'{url}{link}', headers=HEADERS).text
+        link = f"{url}{link}"
+        r = requests.get(link, headers=HEADERS).text
         soup = BeautifulSoup(r, 'lxml')
 
-        para = soup.find('div', id='the_paywall').text
+        para = extract_paratext(soup)
+        text = extract_text(para)
 
-        para_tokenized = tokenizer.tokenize(para)
+        if not text:
+            continue
 
-        for _ in range(10):
-            text = random.choice(para_tokenized)
-            if text and 60 < len(text) < 210:
-                break
-        else:
-            return None
-        return f'{text} {link}'
+        yield f'{text} {link}'
 
 
-
-print(scrape_the_athletic())
+scrape_the_athletic()
